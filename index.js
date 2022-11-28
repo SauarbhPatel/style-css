6ab891396d29960d540af0e85dc74a4f1463e5a1
Saurabh Patel <saurabh123815@gmail.com>
2022-11-28 12:55:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function s1(){
+    let color = document.querySelectorAll('.color')
+
+    color.forEach(color=>{
+        color.style.color='red';
+    })
+}
+function s2(option){
+    let image = document.querySelectorAll('.image')
+    if (option.shadow_type === 'hard') {
+        option.shadow_type = '0px'
+    }
+    else {
+        option.shadow_type = '20px'
+    }
+    color.forEach(image=>{
+        image.style.boxShadow='10px 10px 10px rgba(0,0,0,0.5)';
+        image.style.padding='10px';
+    })
+}
+module.exports.s1=s1;
