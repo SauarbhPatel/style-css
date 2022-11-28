ad3a9cf963cae1c60b7e69a81820725bf0f0929c
Saurabh Patel <saurabh123815@gmail.com>
2022-11-28 14:34:32 +0530

    1

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-function s1(){
-    let color = document.querySelectorAll('.color')
+function css() {
+  let color = document.querySelectorAll(".color");
 
-    color.forEach(color=>{
-        color.style.color='red';
-    })
+  color.forEach((color) => {
+    color.style.color = "red";
+  });
+
+  let image = document.querySelectorAll(".image");
+ 
+  image.forEach((image) => {
+    image.style.boxShadow = "10px 10px 10px rgba(0,0,0,0.5)";
+    image.style.padding = "10px";
+  });
 }
-function s2(option){
-    let image = document.querySelectorAll('.image')
-    if (option.shadow_type === 'hard') {
-        option.shadow_type = '0px'
-    }
-    else {
-        option.shadow_type = '20px'
-    }
-    image.forEach(image=>{
-        image.style.boxShadow='10px 10px 10px rgba(0,0,0,0.5)';
-        image.style.padding='10px';
-    })
-}
-module.exports.s2=s2;
+css();
+module.exports.css = css;
